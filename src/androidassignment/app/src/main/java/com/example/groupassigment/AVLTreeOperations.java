6118e43acde75da48d8b6a0ee85b9ec9a7276cd5
Jiayuan Huang <u7416617@anu.edu.au>
2023-05-09 22:09:44 +1000

    AVL ADDED

--- src/androidassignment/app/src/main/java/com/example/groupassigment/AVLTreeOperations.java
+++ src/androidassignment/app/src/main/java/com/example/groupassigment/AVLTreeOperations.java
@@ -10,10 +10,10 @@ import java.nio.file.Paths;
 
 public class AVLTreeOperations {
 
-    private static String absolutePath = new File("").getAbsolutePath();
-    private static String ANUpath = absolutePath + "/app/src/main/java/ANU.json";
-    private static String Fieldpath = absolutePath + "/app/src/main/java/Field_code.json";
-    private static String collagepath = absolutePath + "/app/src/main/java/Fied_collage.json";
+    public static String absolutePath = new File("").getAbsolutePath();
+    public static String ANUpath = absolutePath + "/app/src/main/java/ANU.json";
+    public static String Fieldpath = absolutePath + "/app/src/main/java/Field_code.json";
+    public static String collagepath = absolutePath + "/app/src/main/java/Fied_collage.json";
     public static AVLTree.Node search(String jsonFile, int searchKey) throws IOException {
         // Read JSON from file
         String json = new String(Files.readAllBytes(Paths.get(jsonFile)));
@@ -26,85 +26,54 @@ public class AVLTreeOperations {
         return avlTree.search(avlTree.root, searchKey);
     }
 
-    public static boolean updatStu(String jsonFile, int searchKey, String student) throws IOException {
-        String json = new String(Files.readAllBytes(Paths.get(jsonFile)));
+    public static boolean updatStu(AVLTree tree, int searchKey, String student) throws IOException {
 
-        // Deserialize JSON into AVLTree object
-        Gson gson = new Gson();
-        AVLTree avlTree = gson.fromJson(json, AVLTree.class);
-
-        // Update the tree using the key
-        boolean updated = avlTree.update_student(avlTree.root, searchKey, student);
+        boolean updated = tree.update_student(tree.root, searchKey, student);
 
         if (updated) {
             // Serialize the updated AVL tree to JSON
             Gson gson2 = new GsonBuilder().setPrettyPrinting().create();
-            String updatedJson = gson2.toJson(avlTree);
+            String updatedJson = gson2.toJson(tree);
 
             // Write the updated JSON to file
-            Files.write(Paths.get(jsonFile), updatedJson.getBytes());
+            Files.write(Paths.get(ANUpath), updatedJson.getBytes());
+        }
+
+        return updated;
+    }
+
+    public static boolean updateFil(AVLTree tree, int searchKey, String file) throws IOException {
+
+
+
+        // Update the tree using the key
+        boolean updated = tree.update_file(tree.getRoot(), searchKey, file);
+
+        if (updated) {
+            // Serialize the updated AVL tree to JSON
+            Gson gson2 = new GsonBuilder().setPrettyPrinting().create();
+            String updatedJson = gson2.toJson(tree);
+
+            // Write the updated JSON to file
+            Files.write(Paths.get(ANUpath), updatedJson.getBytes());
         }
 
 
         return updated;
     }
 
-    public static boolean updateFil(String jsonFile, int searchKey, String file) throws IOException {
+
+    public static AVLTree CreatTree(String jsonFile) throws IOException {
+        // Read JSON from file
         String json = new String(Files.readAllBytes(Paths.get(jsonFile)));
 
         // Deserialize JSON into AVLTree object
         Gson gson = new Gson();
         AVLTree avlTree = gson.fromJson(json, AVLTree.class);
 
-        // Update the tree using the key
-        boolean updated = avlTree.update_file(avlTree.root, searchKey, file);
-
-        if (updated) {
-            // Serialize the updated AVL tree to JSON
-            Gson gson2 = new GsonBuilder().setPrettyPrinting().create();
-            String updatedJson = gson2.toJson(avlTree);
-
-            // Write the updated JSON to file
-            Files.write(Paths.get(jsonFile), updatedJson.getBytes());
-        }
-
-
-        return updated;
+        // Return the root of the tree
+        return avlTree;
     }
 
-    public static void main(String[] args) throws IOException {
 
-        String jsonFile = ANUpath;
-        System.out.println(ANUpath);
-        int searchKey = -1891117552;
-        AVLTree.Node node = search(jsonFile, searchKey);
-
-        // Print the result
-        if (node != null) {
-            System.out.println("Key: " + node.value);
-            System.out.println("Data: " + node.A);
-            System.out.println("Number: " + node.NUMBER);
-        } else {
-            System.out.println("Key not found.");
-        }
-
-        String student = "u7416619";
-        boolean updated = updatStu(jsonFile, searchKey, student);
-
-        if (updated) {
-            System.out.println("Node updated successfully.");
-        } else {
-            System.out.println("Node with the given key not found.");
-        }
-
-
-        String file = "https://firebasestorage.googleapis.com/v0/b/silken-sled-356305.appspot.com/o/%E5%B7%A5%E4%BD%9C%E5%AE%A4MOD%E7%BB%84%EF%BC%88AI-HS2%EF%BC%89.txt?alt=media&token=2bf4ea93-6c0c-4ddb-90d6-60d022153d55";
-        boolean updated2 = updateFil(jsonFile, searchKey, file);
-
-        if (updated2) {
-            System.out.println("Node updated successfully.");
-        } else {
-            System.out.println("Node with the given key not found.");
-        }
-    }
 }
--- src/androidassignment/app/src/main/java/com/example/groupassigment/dashboardMain.java
+++ src/androidassignment/app/src/main/java/com/example/groupassigment/dashboardMain.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,21 @@ public class dashboardMain extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard_main);
 
         DrawerLayout drawerLayout = findViewById(R.id.drawerlayout);
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.courseOption:
+                    System.out.println("courseOption_open");
+                    break;
+                case R.id.userProfile:
+                    break;
+                case R.id.newItem:
+                    break;
+            }
+            return true;
+        });
+
         findViewById(R.id.dashMenu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,7 +41,7 @@ public class dashboardMain extends AppCompatActivity {
             }
         });
 
-        NavigationView navigationView = findViewById(R.id.navigationView);
+
 
     }
 
